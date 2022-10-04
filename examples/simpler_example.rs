addaa0dbfa5f06b43f635e845e1c5e726c824fd0
Igor Schiessl Froehner <igoor.sf14@gmail.com>
2022-10-04 19:49:10 -0300

    feat: simpler example

---
+++ examples/simpler_example.rs
@@ -0,0 +1,41 @@
+
+use bevy::prelude::*;
+use bevy::window::PresentMode;
+
+use ants::ants::AntPlugin;
+
+const BACKGROUND_COLOR: Color = Color::rgb(0.1, 0.4, 0.1);
+
+const WINDOW_WIDTH: f32 = 800.0;
+const WINDOW_HEIGHT: f32 = 800.0;
+
+fn main() {
+    println!("Running main example!");
+
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Ants".to_string(),
+            width: WINDOW_WIDTH,
+            height: WINDOW_HEIGHT,
+            resizable: false,
+            present_mode: PresentMode::AutoNoVsync,
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(AntPlugin {
+            n_ants: 3,
+            n_food: 5,
+            board_size: 10,
+            ant_timer: 0.01,
+            iterations_per_frame: 1,
+            max_iterations: 1_000,
+            radius: 1,
+        })
+        .insert_resource(ClearColor(BACKGROUND_COLOR))
+        .add_startup_system(start_camera)
+        .run();
+}
+
+fn start_camera(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle::default());
+}
